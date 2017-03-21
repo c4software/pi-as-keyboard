2b9409cc7481fe14c4a8ebf22831443966eae943
Brosseau Valentin <c4software@gmail.com>
2017-03-21 21:45:48 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Test if is Root
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
+
+# Enable dwc2 on the Pi
+echo "dtoverlay=dwc2" | sudo tee -a /boot/config.txt
+
+# Enable dwc2 initialisation
+echo "dwc2" | sudo tee -a /etc/modules
+
+# Install service
+cp enable_hid.service /lib/systemd/system/
+
+# Install script
+cp enable_hid.sh /usr/bin/
+
+# Enable service
+systemctl enable enable_hid.service
