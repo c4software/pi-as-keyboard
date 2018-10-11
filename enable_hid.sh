499ba9b466af329f4002e6a96bf4c72de35d9f4b
Brosseau Valentin <c4software@gmail.com>
2018-10-11 22:45:44 +0200

    Add mouse definition

--- enable_hid.sh
+++ enable_hid.sh
@@ -19,14 +19,23 @@ echo "Valentin Brosseau" > strings/0x409/manufacturer
 echo "Keyboard-dev" > strings/0x409/product
 echo 250 > configs/c.1/MaxPower
 
-# Keyboard definition
+# Define a Keyboard
 mkdir -p functions/hid.usb0
 echo 1 > functions/hid.usb0/protocol
 echo 1 > functions/hid.usb0/subclass
 echo 8 > functions/hid.usb0/report_length
 echo -ne \\x05\\x01\\x09\\x06\\xa1\\x01\\x05\\x07\\x19\\xe0\\x29\\xe7\\x15\\x00\\x25\\x01\\x75\\x01\\x95\\x08\\x81\\x02\\x95\\x01\\x75\\x08\\x81\\x03\\x95\\x05\\x75\\x01\\x05\\x08\\x19\\x01\\x29\\x05\\x91\\x02\\x95\\x01\\x75\\x03\\x91\\x03\\x95\\x06\\x75\\x08\\x15\\x00\\x25\\x65\\x05\\x07\\x19\\x00\\x29\\x65\\x81\\x00\\xc0 > functions/hid.usb0/report_desc
+ln -s functions/hid.usb0 configs/c.1/
+
+# Define a mouse
+# mkdir -p functions/hid.usb1
+# echo 1 > functions/hid.usb1/protocol
+# echo 1 > functions/hid.usb1/subclass
+# echo 3 > functions/hid.usb1/report_length
+# echo -ne \\x05\\x01\\x09\\x02\\xa1\\x01\\x09\\x01\\xa1\\x00\\x05\\x09\\x19\\x01\\x29\\x03\\x15\\x00\\x25\\x01\\x95\\x03\\x75\\x01\\x81\\x02\\x95\\x01\\x75\\x05\\x81\\x03\\x05\\x01\\x09\\x30\\x09\\x31\\x15\\x81\\x25\\x7f\\x75\\x08\\x95\\x02\\x81\\x06\\xc0\\xc0 > functions/hid.usb1/report_desc
+# ln -s functions/hid.usb1 configs/c.1/
+
+
 mkdir -p configs/c.1/strings/0x409
 echo "Config 1: ECM network" > configs/c.1/strings/0x409/configuration
-
-ln -s functions/hid.usb0 configs/c.1/
 ls /sys/class/udc > UDC
