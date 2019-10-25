04776bf378b0f454ac8a07604877674ef42abeec
Brosseau Valentin <c4software@gmail.com>
2019-10-25 20:38:57 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,6 +5,6 @@ if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
 for run in {1..10}
 do
-	echo -ne "\x00\x00\x00\x17\x00\x00\x00\x00" > /dev/hidg0
+	echo -ne "\x00\x00\x04\x00\x00\x00\x00\x00" > /dev/hidg0
 	echo -ne "\x00\x00\x00\x00\x00\x00\x00\x00"  > /dev/hidg0
 done
